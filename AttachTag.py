9839a7a664060849ba8b7908925b1d1fa5441c31
AnthonyWuZYU <78756543+AnthonyWuZYU@users.noreply.github.com>
2021-04-21 14:46:13 -0400

    Added text tag for the attachment box

---
+++ AttachTag.py
@@ -0,0 +1,35 @@
+# All works in this code have been curated by ECCC and licensed under the GNU General Public License v3.0.
+# Read more: https://www.gnu.org/licenses/gpl-3.0.en.html
+
+import wx
+
+class AttachTag(wx.Panel):
+    def __init__(self, tag, subtag, *args, **kwargs):
+        super(AttachTag, self).__init__(*args, **kwargs)
+        self.tag = tag
+        self.sub = subtag
+        self.InitUI()
+
+    def InitUI(self):
+
+        layoutSizer = wx.BoxSizer(wx.VERTICAL)
+
+        self.noteHeader = wx.StaticText(self, label=self.tag)
+        self.subTag = wx.StaticText(self, label=self.sub)
+        layoutSizer.Add(self.noteHeader)
+        layoutSizer.Add(self.subTag)
+
+        self.SetSizer(layoutSizer)
+
+
+def main():
+    app = wx.App()
+
+    frame = wx.Frame(None)
+    AttachTag("DEBUG", frame)
+
+    frame.Show()
+    app.MainLoop()
+
+if __name__ == "__main__":
+    main()
