8aeadddcd82d89ed7b89afd478fef07530fba5f3
AnthonyWuZYU <78756543+AnthonyWuZYU@users.noreply.github.com>
2021-06-02 05:10:43 -0400

    Adjust scrollbar

--- AttachBox.py
+++ AttachBox.py
@@ -43,7 +43,7 @@ class AttachBox(scrolled.ScrolledPanel):
 
         self.layoutSizer.Add(self.columnList[-1])
         self.SetupScrolling()
-        self.ShowScrollbars(wx.SHOW_SB_NEVER, wx.SHOW_SB_ALWAYS)
+        self.ShowScrollbars(wx.SHOW_SB_NEVER, wx.SHOW_SB_DEFAULT)
 
     def check(self):
         for x in self.buttonList:
