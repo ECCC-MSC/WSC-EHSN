9a27e29e0cc82a8373cdf772f73dc39f7c7bc758
AnthonyWuZYU <78756543+AnthonyWuZYU@users.noreply.github.com>
2021-06-02 05:16:29 -0400

    Taking only folders. Adding a checkbox bar for the content in the folders

--- AttachFolderBox.py
+++ AttachFolderBox.py
@@ -6,20 +6,19 @@ import os
 import sys
 import wx.lib.scrolledpanel as scrolled
 
-class AttachFolderBox(scrolled.ScrolledPanel):
+class AttachFolderBox(wx.Panel):
     def __init__(self, parent, func, *args, **kwargs):
         super(AttachFolderBox, self).__init__(*args, **kwargs)
         self.parent = parent
         self.func = func
         self.manager = None
-        self.barSize = (645, -1)
+        self.barSize = (710, -1)
         self.buttonSize = (30, -1)
         self.buttonList = []
         self.addrList = []
         self.browseList = []
         self.columnList = []
-        self.typeList = []
-        self.type = ["File", "Folder"]
+        self.pathList = []
         self.rootPath = os.path.dirname(os.path.realpath(sys.argv[0]))
 
         self.InitUI()
@@ -28,26 +27,69 @@ class AttachFolderBox(scrolled.ScrolledPanel):
 
         self.layoutSizer = wx.BoxSizer(wx.VERTICAL)
         self.SetSizer(self.layoutSizer)
-
+        
+        self.pathPanel = scrolled.ScrolledPanel(self, style = wx.NO_BORDER)
+        self.pathSizer = wx.BoxSizer(wx.VERTICAL)
+        self.pathPanel.SetSizer(self.pathSizer)
+        
         self.columnList.append(wx.BoxSizer(wx.HORIZONTAL))
 
-        self.buttonList.append(wx.Button(self, label="+", size=self.buttonSize))
-        self.typeList.append(wx.ComboBox(self, style=wx.CB_DROPDOWN|wx.TE_READONLY, choices=self.type))
-        self.typeList[-1].SetValue("File")
-        self.addrList.append(wx.TextCtrl(self, size=self.barSize))
-        self.browseList.append(wx.Button(self, label="Browse"))
+        self.buttonList.append(wx.Button(self.pathPanel, label="+", size=self.buttonSize))
+        self.addrList.append(wx.TextCtrl(self.pathPanel, size=self.barSize))
+        self.browseList.append(wx.Button(self.pathPanel, label="Browse"))
+        self.pathList.append("")
 
         self.buttonList[-1].Bind(wx.EVT_BUTTON, self.add_remove)
         self.browseList[-1].Bind(wx.EVT_BUTTON, self.Browse)
 
         self.columnList[-1].Add(self.buttonList[-1])
-        self.columnList[-1].Add(self.typeList[-1])
         self.columnList[-1].Add(self.addrList[-1])
         self.columnList[-1].Add(self.browseList[-1])
 
-        self.layoutSizer.Add(self.columnList[-1])
-        self.SetupScrolling()
-        self.ShowScrollbars(wx.SHOW_SB_NEVER, wx.SHOW_SB_ALWAYS)
+        self.pathSizer.Add(self.columnList[-1])
+        self.layoutSizer.Add(self.pathPanel, 1, wx.EXPAND)
+        
+        self.spit = wx.Panel(self, style = wx.SIMPLE_BORDER, size = (-1, 1))
+        self.layoutSizer.Add(self.spit, 0, wx.EXPAND)
+        self.layoutSizer.Add((-1, 4))
+        
+        self.includePanel = wx.Panel(self, size = (-1, 20))
+        self.inlcudeRow = wx.BoxSizer(wx.HORIZONTAL)
+        self.includePanel.SetSizer(self.inlcudeRow)
+        contentLabel = wx.StaticText(self.includePanel, label = "Content of Logger Folder:")
+        
+        self.dataCheck = wx.CheckBox(self.includePanel)
+        self.dataCheck.Bind(wx.EVT_CHECKBOX, self.notEmpty)
+        dataLabel = wx.StaticText(self.includePanel, label = "Data Files")
+       
+        self.diagnosticCheck = wx.CheckBox(self.includePanel)
+        self.diagnosticCheck.Bind(wx.EVT_CHECKBOX, self.notEmpty)
+        diagnosticLabel = wx.StaticText(self.includePanel, label="Diagnostic Files")
+        
+        self.programCheck = wx.CheckBox(self.includePanel)
+        self.programCheck.Bind(wx.EVT_CHECKBOX, self.notEmpty)
+        programLabel = wx.StaticText(self.includePanel, label="Program Files")
+        
+        self.inlcudeRow.Add((5, -1))
+        self.inlcudeRow.Add(contentLabel)
+        self.inlcudeRow.Add((60,-1))
+        self.inlcudeRow.Add(self.dataCheck)
+        self.inlcudeRow.Add(dataLabel)
+        self.inlcudeRow.Add((80, -1))
+        self.inlcudeRow.Add(self.diagnosticCheck)
+        self.inlcudeRow.Add(diagnosticLabel)
+        self.inlcudeRow.Add((80, -1))
+        self.inlcudeRow.Add(self.programCheck)
+        self.inlcudeRow.Add(programLabel)
+        
+        self.layoutSizer.Add(self.includePanel, 0, wx.EXPAND)
+        self.pathPanel.SetupScrolling()
+        self.pathPanel.ShowScrollbars(wx.SHOW_SB_NEVER, wx.SHOW_SB_DEFAULT)
+
+    def notEmpty(self, evt):
+        if len(self.returnPath()) == 0:
+            evt.GetEventObject().SetValue(False)
+        evt.Skip()
 
     def check(self):
         for x in self.buttonList:
@@ -58,20 +100,18 @@ class AttachFolderBox(scrolled.ScrolledPanel):
 
     def add(self):
         self.columnList.append(wx.BoxSizer(wx.HORIZONTAL))
-        self.buttonList.append(wx.Button(self, label="+", size=self.buttonSize))
-        self.typeList.append(wx.ComboBox(self, style=wx.CB_DROPDOWN | wx.TE_READONLY, choices=self.type))
-        self.typeList[-1].SetValue("File")
-        self.addrList.append(wx.TextCtrl(self, size=self.barSize))
-        self.browseList.append(wx.Button(self, label="Browse"))
+        self.buttonList.append(wx.Button(self.pathPanel, label="+", size=self.buttonSize))
+        self.addrList.append(wx.TextCtrl(self.pathPanel, size=self.barSize))
+        self.browseList.append(wx.Button(self.pathPanel, label="Browse"))
+        self.pathList.append("")
 
         self.buttonList[-1].Bind(wx.EVT_BUTTON, self.add_remove)
         self.browseList[-1].Bind(wx.EVT_BUTTON, self.Browse)
         self.columnList[-1].Add(self.buttonList[-1])
-        self.columnList[-1].Add(self.typeList[-1])
         self.columnList[-1].Add(self.addrList[-1])
         self.columnList[-1].Add(self.browseList[-1])
 
-        self.layoutSizer.Add(self.columnList[-1])
+        self.pathSizer.Add(self.columnList[-1])
         self.check()
         self.layoutSizer.Layout()
 
@@ -80,20 +120,18 @@ class AttachFolderBox(scrolled.ScrolledPanel):
     def add_remove(self, evt):
         if evt.GetEventObject().GetLabel() == "+":
             self.columnList.append(wx.BoxSizer(wx.HORIZONTAL))
-            self.buttonList.append(wx.Button(self, label="+", size=self.buttonSize))
-            self.typeList.append(wx.ComboBox(self, style=wx.CB_DROPDOWN | wx.TE_READONLY, choices=self.type))
-            self.typeList[-1].SetValue("File")
-            self.addrList.append(wx.TextCtrl(self, size=self.barSize))
-            self.browseList.append(wx.Button(self, label="Browse"))
+            self.buttonList.append(wx.Button(self.pathPanel, label="+", size=self.buttonSize))
+            self.addrList.append(wx.TextCtrl(self.pathPanel, size=self.barSize))
+            self.browseList.append(wx.Button(self.pathPanel, label="Browse"))
+            self.pathList.append("")
 
             self.buttonList[-1].Bind(wx.EVT_BUTTON, self.add_remove)
             self.browseList[-1].Bind(wx.EVT_BUTTON, self.Browse)
             self.columnList[-1].Add(self.buttonList[-1])
-            self.columnList[-1].Add(self.typeList[-1])
             self.columnList[-1].Add(self.addrList[-1])
             self.columnList[-1].Add(self.browseList[-1])
 
-            self.layoutSizer.Add(self.columnList[-1])
+            self.pathSizer.Add(self.columnList[-1])
             self.check()
             self.layoutSizer.Layout()
 
@@ -102,14 +140,18 @@ class AttachFolderBox(scrolled.ScrolledPanel):
         elif evt.GetEventObject().GetLabel() == "-":
             id = self.buttonList.index(evt.GetEventObject())
             self.buttonList[id].Destroy()
-            self.typeList[id].Destroy()
             self.addrList[id].Destroy()
             self.browseList[id].Destroy()
             del self.columnList[id]
             del self.buttonList[id]
-            del self.typeList[id]
             del self.addrList[id]
             del self.browseList[id]
+            del self.pathList[id]
+
+            if len(self.returnPath()) == 0:
+                self.dataCheck.SetValue(False)
+                self.diagnosticCheck.SetValue(False)
+                self.programCheck.SetValue(False)
 
             self.check()
             self.layoutSizer.Layout()
@@ -118,43 +160,18 @@ class AttachFolderBox(scrolled.ScrolledPanel):
 
     def Browse(self, evt):
         id = self.browseList.index(evt.GetEventObject())
-        if self.typeList[id].GetValue() == "Folder":
-            DirOpenDialog = wx.DirDialog(self, "Select the Folder", self.rootPath,
-                                         style=wx.DD_DEFAULT_STYLE | wx.DD_DIR_MUST_EXIST)
-            if DirOpenDialog.ShowModal() == wx.ID_CANCEL:
-                DirOpenDialog.Destroy()
-                return
+        folderOpenDialog = wx.DirDialog(self, "Select the File", self.rootPath, 
+                                        style=wx.DD_DEFAULT_STYLE | wx.DD_DIR_MUST_EXIST)
+        if folderOpenDialog.ShowModal() == wx.ID_CANCEL:
+            folderOpenDialog.Destroy()
+            return
 
-            self.addrList[id].ChangeValue(DirOpenDialog.GetPath())
+        self.pathList[id] = folderOpenDialog.GetPath()
+        self.addrList[id].ChangeValue(self.pathList[id])
 
-            DirOpenDialog.Destroy()
+        folderOpenDialog.Destroy()
 
-        else:
-            fileOpenDialog = wx.FileDialog(self, "Select the File", self.rootPath, '',
-                                           self.func,
-                                           style=wx.FD_OPEN | wx.FD_CHANGE_DIR)
-            if fileOpenDialog.ShowModal() == wx.ID_CANCEL:
-                fileOpenDialog.Destroy()
-                return
-
-            self.addrList[id].ChangeValue(fileOpenDialog.GetPath())
-
-            fileOpenDialog.Destroy()
-
-    def returnFilePath(self):
-        fileList = []
-        for x in range(len(self.addrList)):
-            if self.typeList[x].GetValue() == "File" and self.addrList[x].GetValue() != "":
-                fileList.append(self.addrList[x].GetValue())
-        return fileList
-
-    def returnFolderPath(self):
-        folderList = []
-        for x in range(len(self.addrList)):
-            if self.typeList[x].GetValue() == "Folder" and self.addrList[x].GetValue() != "":
-                folderList.append(self.addrList[x].GetValue())
-        return folderList
-
+    # return none empty path
     def returnPath(self):
         pathList = []
         for x in range(len(self.addrList)):
